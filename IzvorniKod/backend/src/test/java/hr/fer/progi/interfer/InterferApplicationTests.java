7a4bbcaa67b8044f3b7264e235f0d8899f217d06
Antonia <as54033@fer.hr>
2023-11-03 15:14:00 +0100

    feat: init backend

---
+++ IzvorniKod/backend/src/test/java/hr/fer/progi/interfer/InterferApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.progi.interfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
