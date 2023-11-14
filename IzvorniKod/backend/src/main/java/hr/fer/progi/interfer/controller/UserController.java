701e1f7f2fea25a3cbb72c812ccb880b19d939dc
Antonia <as54033@fer.hr>
2023-11-14 09:42:23 +0100

    feat: add login backend

--- IzvorniKod/backend/src/main/java/hr/fer/progi/interfer/controller/UserController.java
+++ IzvorniKod/backend/src/main/java/hr/fer/progi/interfer/controller/UserController.java
@@ -1,11 +1,14 @@
 package hr.fer.progi.interfer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import hr.fer.progi.interfer.dto.request.UserLoginDTO;
 import hr.fer.progi.interfer.dto.request.UserRegistrationDTO;
+import hr.fer.progi.interfer.service.impl.UserLoginServiceImpl;
 import hr.fer.progi.interfer.service.impl.UserRegisterServiceImpl;
 import jakarta.validation.Valid;
 
@@ -15,18 +18,29 @@ import jakarta.validation.Valid;
 public class UserController {
 	
 	@Autowired
-    private UserRegisterServiceImpl userService;
+    private UserRegisterServiceImpl userRegisterService;
+	
+	@Autowired
+    private UserLoginServiceImpl userLoginService;
 	
 	@PostMapping("/register")
     public ResponseEntity<?> registerUser(@ModelAttribute @Valid UserRegistrationDTO userDetails, BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
             return ResponseEntity.badRequest().body(bindingResult.toString());
         }
-    	return userService.register(userDetails);
+    	return userRegisterService.register(userDetails);
     }
 	
-	@GetMapping("/")
-    public ResponseEntity<?> profile() {
+	@PostMapping("/login")
+    public ResponseEntity<?> loginUser(@ModelAttribute @Valid UserLoginDTO userDetails, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.toString());
+        }
+    	return userLoginService.login(userDetails);
+    }
+	
+	@GetMapping()
+    public ResponseEntity<?> userProfile(@RequestHeader HttpHeaders header) {
     	return ResponseEntity.ok().body("User");		//nije implementirano do kraja, za test
     }
 
