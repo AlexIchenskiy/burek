6d5f032e649a57a3944574aa3f0ef2da08fa9d3a
AlexIchenskiy <aichenskiy@gmail.com>
2023-11-15 17:48:32 +0100

    feat: connect forms to backend

--- IzvorniKod/frontend/src/pages/Register/register.js
+++ IzvorniKod/frontend/src/pages/Register/register.js
@@ -1,22 +1,45 @@
-import React, {useState} from 'react';
-import { TextField, Button, Container, Stack } from '@mui/material';
-import { Link } from "react-router-dom"
-import "./register.css"
- 
+import React, { useState } from 'react';
+import { TextField, Button, Snackbar, Alert } from '@mui/material';
+import { Link, useNavigate } from "react-router-dom";
+import "./register.css";
+import axios from 'axios';
+import { API_URL } from '../../assets/constants';
+import useAuth from '../../hooks/useAuth';
+
 const RegisterForm = () => {
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [email, setEmail] = useState('')
-    const [dateOfBirth, setDateOfBirth] = useState('')
-    const [password, setPassword] = useState('')
- 
+    const { setToken } = useAuth();
+    const navigate = useNavigate();
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [openSnackbar, setOpenSnackbar] = useState(false);
+    const [snackbarMessage, setSnackbarMessage] = useState("");
+
+    const handleSnackbarClose = () => {
+        setOpenSnackbar(false);
+    };
+
     function handleSubmit(event) {
         event.preventDefault();
-        console.log(firstName, lastName, email, dateOfBirth, password) 
+        console.log(firstName, lastName, email, password);
+
+        axios.post(`${API_URL}/user/register`, {
+            firstname: firstName, lastname: lastName, email: email, password: password
+        })
+            .then((res) => {
+                setToken(res.token);
+                navigate("/home", { replace: true });
+            })
+            .catch((err) => {
+                setSnackbarMessage(err + "");
+                setOpenSnackbar(true)
+            });
     }
- 
+
     return (
-        <React.Fragment>
+        <>
             <form method="POST" action="/api/user/register" onSubmit={handleSubmit} >
                 <h2>Register Form</h2>
                 <TextField
@@ -28,7 +51,7 @@ const RegisterForm = () => {
                     value={firstName}
                     fullWidth
                     required
-                    sx={{mb: 4}}
+                    sx={{ mb: 4 }}
                 />
                 <TextField
                     type="text"
@@ -39,7 +62,7 @@ const RegisterForm = () => {
                     value={lastName}
                     fullWidth
                     required
-                    sx={{mb: 4}}
+                    sx={{ mb: 4 }}
                 />
                 <TextField
                     type="email"
@@ -50,7 +73,7 @@ const RegisterForm = () => {
                     value={email}
                     fullWidth
                     required
-                    sx={{mb: 4}}
+                    sx={{ mb: 4 }}
                 />
                 <TextField
                     type="password"
@@ -61,25 +84,22 @@ const RegisterForm = () => {
                     value={password}
                     required
                     fullWidth
-                    sx={{mb: 4}}
-                />
-                <TextField
-                    type="date"
-                    variant='outlined'
-                    color='primary'
-                    label="Date of Birth"
-                    onChange={e => setDateOfBirth(e.target.value)}
-                    value={dateOfBirth}
-                    fullWidth
-                    required
-                    sx={{mb: 4}}
+                    sx={{ mb: 4 }}
                 />
                 <Button variant="outlined" color="primary" type="submit">Register</Button>
                 <small>Already have an account? <Link to="/login">Login Here</Link></small>
             </form>
-     
-        </React.Fragment>
+            <Snackbar
+                open={openSnackbar}
+                autoHideDuration={6000}
+                onClose={handleSnackbarClose}
+            >
+                <Alert onClose={handleSnackbarClose} severity="error">
+                    {snackbarMessage}
+                </Alert>
+            </Snackbar>
+        </>
     )
 }
- 
+
 export default RegisterForm;
