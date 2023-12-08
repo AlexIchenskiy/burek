2ce270666d7f80d31a0b3a82616f6b1354e2b3f3
Antonia <as54033@fer.hr>
2023-12-08 22:27:14 +0100

    fix: change request type

--- IzvorniKod/backend/src/main/java/hr/fer/progi/interfer/controller/CommentController.java
+++ IzvorniKod/backend/src/main/java/hr/fer/progi/interfer/controller/CommentController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -35,7 +36,7 @@ public class CommentController {
     	return commentPostService.post(authorizationHeader, commentDetails);
     }
 	
-	@PostMapping("/getAll")
+	@GetMapping("/getAll")
     public ResponseEntity<?> getAllComments(@RequestBody @Valid ArticleGetDTO articleDetails, BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
             return ResponseEntity.badRequest().body(bindingResult.toString());
