6300886d42d5ed607a0ab0a8c09bac296cc94a4d
AlexIchenskiy <aichenskiy@gmail.com>
2024-01-02 14:30:16 +0100

    feat: add article sharing

--- IzvorniKod/frontend/src/pages/Post/Post.js
+++ IzvorniKod/frontend/src/pages/Post/Post.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Editor as TextEditor, EditorState, convertFromRaw, ContentState } from 'draft-js';
 import 'draft-js/dist/Draft.css';
+import { EmailShareButton, EmailIcon, FacebookIcon, FacebookShareButton, WhatsappShareButton, WhatsappIcon, LinkedinShareButton, LinkedinIcon } from 'react-share';
 
 import Header from "../../components/Header/Header";
 
@@ -8,7 +9,7 @@ import * as S from "./PostStyles";
 import "./DraftStyles.css";
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { API_URL } from '../../assets/constants';
+import { API_URL, PAGE_URL } from '../../assets/constants';
 
 const Post = () => {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
@@ -40,6 +41,23 @@ const Post = () => {
           <S.PostTitleInput value={title} type="text" disableUnderline={true} placeholder='Ovdje još nema naslova :(' readOnly={true} />
           <TextEditor editorState={editorState} onChange={setEditorState} placeholder='Ovjde još nema ništa :(' readOnly={true} />
         </S.PostTextContainer>
+        <S.PostShareContainer>
+          <FacebookShareButton url={`${PAGE_URL}/post/${id}`}>
+            <FacebookIcon size={32} round />
+          </FacebookShareButton>
+          <LinkedinShareButton url={`${PAGE_URL}/post/${id}`}>
+            <LinkedinIcon size={32} round />
+          </LinkedinShareButton>
+          <WhatsappShareButton url={`${PAGE_URL}/post/${id}`}>
+            <WhatsappIcon size={32} round />
+          </WhatsappShareButton>
+          <S.CopyShareButton onClick={() => { navigator.clipboard.writeText(`${PAGE_URL}/post/${id}`) }}>
+            <S.CopyShareIcon />
+          </S.CopyShareButton>
+          <EmailShareButton url={`${PAGE_URL}/post/${id}`}>
+            <EmailIcon size={32} round />
+          </EmailShareButton>
+        </S.PostShareContainer>
       </S.PostContainer>
     </>
   )
