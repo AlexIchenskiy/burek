baf3d926abf2f6c2c09e2267f551b0343d46c253
Anton Ladan <102412343+AL-Man64@users.noreply.github.com>
2024-01-17 19:42:43 +0100

    feat: add unit tests

--- IzvorniKod/backend/src/test/java/hr/fer/progi/interfer/InterferApplicationTests.java
+++ IzvorniKod/backend/src/test/java/hr/fer/progi/interfer/InterferApplicationTests.java
@@ -1,5 +1,5 @@
 package hr.fer.progi.interfer;
-/*
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,4 +11,3 @@ class InterferApplicationTests {
 	}
 
 }
-*/
